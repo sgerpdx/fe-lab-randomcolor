f86147e65173a2b0a4b116f437329a032da81040
SamuelHGerber <henrylightfoot@gmail.com>
2021-05-10 17:16:45 -0700

    DisplayColor div hard-coded to test color render

--- src/components/app/Display/DisplayColor.jsx
+++ src/components/app/Display/DisplayColor.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 
 export default class DisplayColor extends Component {
   render() {
-    return <div>display shout out</div>;
+    return (
+      <div>
+        display shout out
+        <div
+          style={{ background: '#58eb34', height: '40px', width: '40px' }}
+        ></div>
+      </div>
+    );
   }
 }
